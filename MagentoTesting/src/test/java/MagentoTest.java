ab6a11eb788f5d559f7ed39cdaac4e03de93e393
Rakesh <Rakesh@DESKTOP-7209Q0S>
2019-04-04 15:47:32 +0530

    second commit

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest 
@@ -11,7 +11,7 @@ public class MagentoTest
 	@Test
 	public void magentoTest() throws InterruptedException
 	{
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://magento.com");
